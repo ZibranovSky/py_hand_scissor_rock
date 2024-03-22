57d512d9eea1fe4f48982a488e509aff6df87f59
ZibranovSky <katyushachan191202@gmail.com>
2024-03-23 06:52:53 +0700

    update script

--- hand_scissor_rock.py
+++ hand_scissor_rock.py
@@ -6,37 +6,68 @@ import time
 print("Simple hand scissor and rock game \n")
 word = ["scissor","hand","rock"]
 
-player_choice = input("Enter your choice (hand/scissor/rock) : \n")
+
+repeat = "y"
+
+while repeat != "n":
+    
+    player_choice = input("Enter your choice (hand/scissor/rock) : \n")
 
 
-computer_choice = rand.choice(word)
+    print("\n")
 
-print("wait while your enemy choice its decision....")
+    computer_choice = rand.choice(word)
 
-time.sleep(2)
+    print("wait while your enemy choice....")
 
-print("your enemy release {} ".format(computer_choice))
+    time.sleep(2)
 
-if player_choice == "hand" and computer_choice == "hand":
-    print("Draw")
-elif player_choice == "hand" and computer_choice == "scissor":
-    print("You lose to your enemy")
-elif player_choice == "hand" and computer_choice == "rock":
-    print("You win!!")
-elif player_choice == "scissor" and computer_choice == "hand":
-    print("You win!!")
-elif player_choice == "scissor" and computer_choice == "scissor":
-    print("Draw")
-elif player_choice == "scissor" and computer_choice == "rock":
-    print("You lose to your enemy")
-elif player_choice == "rock" and computer_choice == "hand":
-    print("You lose to your enemy")
-elif player_choice == "rock" and computer_choice == "scissor":
-    print("You win!!")
-elif player_choice == "rock" and computer_choice == "rock":
-    print("Draw")
-else:
-    print("wrong input!")
+    print("your enemy release {} ".format(computer_choice))
 
+    if player_choice == "hand" and computer_choice == "hand":
+        print("Draw")
+        print("\n")
+        repeat = input("repeat program ? (y/n) ")
+    elif player_choice == "hand" and computer_choice == "scissor":
+        print("You lose to your enemy")
+        print("\n")
+        repeat = input("repeat program ? (y/n) ")
+    elif player_choice == "hand" and computer_choice == "rock":
+        print("You win!!")
+        print("\n")
+        repeat = input("repeat program ? (y/n) ")
+    elif player_choice == "scissor" and computer_choice == "hand":
+        print("You win!!")
+        print("\n")
+        repeat = input("repeat program ? (y/n) ")
+    elif player_choice == "scissor" and computer_choice == "scissor":
+        print("Draw")
+        print("\n")
+        repeat = input("repeat program ? (y/n) ")
+    elif player_choice == "scissor" and computer_choice == "rock":
+        print("You lose to your enemy")
+        print("\n")
+        repeat = input("repeat program ? (y/n) ")
+    elif player_choice == "rock" and computer_choice == "hand":
+        print("You lose to your enemy")
+        print("\n")
+        repeat = input("repeat program ? (y/n) ")
+    elif player_choice == "rock" and computer_choice == "scissor":
+        print("You win!!")
+        print("\n")
+        repeat = input("repeat program ? (y/n) ")
+    elif player_choice == "rock" and computer_choice == "rock":
+        print("Draw")
+        print("\n")
+        repeat = input("repeat program ? (y/n) ")
+    else:
+        print("wrong input!")
+        print("\n")
+        repeat = input("repeat program ? (y/n) ")
+
+    if repeat == "y":
+        continue
+    else:
+        break
 
 close = input("program end (enter) ")
